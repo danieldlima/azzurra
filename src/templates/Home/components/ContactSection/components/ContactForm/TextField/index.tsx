874946f300bae65d2c0a92b512c7fccd46b87f7c
Daniel Lima <familiasilva.daniel@gmail.com>
2023-01-19 03:25:13 -0300

    refactor(textfield): add label error status

--- src/templates/Home/components/ContactSection/components/ContactForm/TextField/index.tsx
+++ src/templates/Home/components/ContactSection/components/ContactForm/TextField/index.tsx
@@ -3,51 +3,93 @@ import React, { useState } from 'react';
 import { addClassName } from '@root/modules/utils';
 
 interface TextFieldProps {
-  className?: string;
+  classes?: {
+    root?: string;
+    field?: string;
+  };
   name: string;
   type: 'text' | 'email';
   label: string;
+  error?: {
+    text?: string;
+    type?: 'error' | 'warn';
+  } | null;
   required?: boolean;
 }
 
-function TextField({ name, type, label, required, className }: TextFieldProps) {
+function TextField({
+  name,
+  type,
+  label,
+  required,
+  classes,
+  error
+}: TextFieldProps) {
   const [value, setValue] = useState('');
+  const colorLabel = {
+    error: 'border-red-100 text-red-400 bg-red-50',
+    warn: 'border-orange-100 text-orange-400 bg-orange-50'
+  };
+  const borderColor = {
+    error: 'bg-red-600 peer-focus:bg-red-600',
+    warn: 'bg-orange-300 peer-focus:bg-orange-300'
+  };
 
   return (
-    <div className={`relative mt-6 ${addClassName(className)}`}>
-      <label className="block">
-        <input
-          required={required}
-          name={name}
-          type={type}
-          className={
-            'w-full h-12 relative group/field form-input px-0.5 py-0 border-l-0 border-r-0 border-t-0 ' +
-            'focus:ring-transparent focus:border-b focus:border-b-azzurra-gold-100 peer'
-          }
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-        />
+    <>
+      <div className={`relative mt-6${addClassName(classes?.root)}`}>
+        <label className="block">
+          <input
+            required={required}
+            name={name}
+            type={type}
+            className={
+              'w-full h-12 relative group/field form-input px-0.5 py-0 border-l-0 border-r-0 border-t-0 ' +
+              'focus:ring-transparent focus:border-b focus:border-b-azzurra-gold-100 peer'
+            }
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          />
 
-        <span
-          className={`absolute block peer-focus:bottom-12 peer-focus:text-azzurra-gold-100
+          <span
+            className={`absolute block peer-focus:bottom-12 peer-focus:text-azzurra-gold-100
                text-lg peer-focus:text-xs transition-all ${addClassName(
                  value
                    ? 'bottom-12 text-xs text-azzurra-gold-100'
                    : 'bottom-1 text-azzurra-gray-70'
                )}`}
-        >
-          {label}
-        </span>
+          >
+            {label}
+          </span>
 
+          <span
+            className={`absolute w-full h-px bottom-0 peer-focus:h-0.5 block
+              ${addClassName(
+                value
+                  ? 'bg-azzurra-gray-70 peer-focus:bg-azzurra-gold-100'
+                  : error?.type && required
+                  ? addClassName(
+                      error
+                        ? borderColor[error?.type]
+                        : 'bg-azzurra-gray-70 peer-focus:bg-azzurra-gold-100'
+                    )
+                  : ''
+              )}`}
+          />
+        </label>
+      </div>
+
+      {!error || !required ? null : (
         <span
-          className={
-            'absolute w-full h-px bottom-0 peer-focus:h-0.5 block ' +
-            `peer-focus:bg-azzurra-gold-100 bg-azzurra-gray-70`
-          }
-        />
-      </label>
-    </div>
+          className={`text-sm py-2 px-4 border rounded${
+            error?.type && required ? addClassName(colorLabel[error?.type]) : ''
+          }`}
+        >
+          {error?.text}
+        </span>
+      )}
+    </>
   );
 }
 
