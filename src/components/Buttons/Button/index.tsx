10f882706a88d435199821da1883f6b3130b0374
Daniel Lima <familiasilva.daniel@gmail.com>
2023-01-19 17:25:53 -0300

    refactor(button): add onclick listener

--- src/components/Buttons/Button/index.tsx
+++ src/components/Buttons/Button/index.tsx
@@ -3,14 +3,24 @@ import React, { ButtonHTMLAttributes } from 'react';
 import { addClassName } from '@root/modules/utils';
 
 interface ButtonProps {
+  id?: string;
   label?: string;
   className?: string;
   type?: ButtonHTMLAttributes<HTMLButtonElement>['type'];
+  onClick?: ButtonHTMLAttributes<HTMLButtonElement>['onClick'];
 }
 
-function Button({ className, type = 'button', label = 'Button' }: ButtonProps) {
+function Button({
+  className,
+  id,
+  type = 'button',
+  label = 'Button',
+  onClick
+}: ButtonProps) {
   return (
     <button
+      onClick={onClick}
+      id={id}
       type={type}
       className={
         'w-fit bg-azzurra-navy-blue hover:bg-azzurra-gold-100 text-azzurra-opaque-gold ' +
