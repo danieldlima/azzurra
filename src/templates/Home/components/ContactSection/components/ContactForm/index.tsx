cd3f1c1b77372ec052b5c232f735e2350250c6cf
Daniel Lima <familiasilva.daniel@gmail.com>
2023-01-19 17:08:52 -0300

    refactor(contactform): create send mail function to API and create logic to show errors

--- src/templates/Home/components/ContactSection/components/ContactForm/index.tsx
+++ src/templates/Home/components/ContactSection/components/ContactForm/index.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react';
+import { useState } from 'react';
+
+import { EntryTypes, StatusType } from '@constant/api';
+
+import { sendMail } from '@root/modules/utils/send-mail';
+import { ErrorFields } from '@root/types';
 
 import Button from '@components/Buttons/Button';
 import Mail from '@components/Icons/Mail';
@@ -7,7 +13,34 @@ import Text from '@components/Text';
 import TextField from './TextField';
 import TextareaField from './TextareaField';
 
+// { text: 'Email enviado com sucesso!', type: StatusType.Success }
+const initialErrors: ErrorFields = {
+  name: null,
+  email: null,
+  subject: null,
+  generic: null,
+  success: null
+};
+
 function ContactForm() {
+  const [errors, setErrors] = useState<ErrorFields>(initialErrors);
+
+  function handleMessage(msg: string) {
+    const msgError: ErrorFields & { status: StatusType } = JSON.parse(msg);
+
+    setErrors((prevState) => ({ ...prevState, ...msgError }));
+
+    setTimeout(() => {
+      if (msgError.status === StatusType.Success) {
+        setErrors((prevState) => ({ ...prevState, success: null }));
+      }
+    }, 3000);
+  }
+
+  function resetError(name: keyof ErrorFields) {
+    setErrors((prevState) => ({ ...prevState, [name]: null }));
+  }
+
   return (
     <div className="w-full flex flex-col">
       <div
@@ -21,37 +54,80 @@ function ContactForm() {
       <Text color="black" label={'Envie sua mensagem'} />
 
       <div className="flex flex-col">
-        <form id="form-data-contact" className={'w-full'}>
+        <form id="contact_form" className={'w-full'}>
           <div className="flex flex-col gap-4">
             <TextField
+              error={errors.name}
               name="name"
               type="text"
               label="Seu Nome (obrigatório)"
-              className={'w-full'}
+              classes={{
+                root: 'w-full',
+                field: 'form_data'
+              }}
+              onChange={(value) => {
+                if (value) resetError(EntryTypes.Name);
+              }}
               required
             />
 
             <TextField
+              error={errors.email}
               name="email"
               type="email"
               label="Seu E-mail (obrigatório)"
-              className={'w-full'}
+              classes={{
+                root: 'w-full',
+                field: 'form_data'
+              }}
+              onChange={(value) => {
+                if (value) resetError(EntryTypes.Email);
+              }}
               required
             />
 
             <TextField
+              error={errors.subject}
               name="subject"
               type="text"
-              label="Assunto"
-              className={'w-full'}
-              required={false}
+              label="Assunto (obrigatório)"
+              classes={{
+                root: 'w-full',
+                field: 'form_data'
+              }}
+              onChange={(value) => {
+                if (value) resetError(EntryTypes.Subject);
+              }}
+              required
             />
 
             <div className="pt-4 mb-6">
-              <TextareaField name="message" label="Sua Mensagem" />
+              <TextareaField
+                name="message"
+                label="Sua Mensagem"
+                classes={{ field: 'form_data' }}
+              />
             </div>
 
-            <Button type="submit" label="ENVIAR" />
+            {!errors.success ? null : (
+              <span
+                className={
+                  'text-sm py-2 px-4 border rounded border-green-200 text-green-600 bg-green-100'
+                }
+              >
+                {errors.success?.text}
+              </span>
+            )}
+
+            <Button
+              id="submit"
+              type="submit"
+              label="ENVIAR"
+              onClick={(event) => {
+                event.preventDefault();
+                sendMail((msg) => handleMessage(msg));
+              }}
+            />
           </div>
         </form>
       </div>
