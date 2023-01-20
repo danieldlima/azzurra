bae690f66348ad9b905f1d9e62984dd51db57d43
Daniel Lima <familiasilva.daniel@gmail.com>
2023-01-19 22:57:35 -0300

    refactor(processdata): create HTML file templates to send email

---
+++ api/template/with_copy.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
+<head>
+  <title></title>
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+  <!--[if mso]>
+  <xml>
+    <o:OfficeDocumentSettings>
+      <o:PixelsPerInch>96</o:PixelsPerInch>
+      <o:AllowPNG/>
+    </o:OfficeDocumentSettings>
+  </xml><![endif]-->
+  <!--[if !mso]><!-->
+  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Shrikhand" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css"/>
+  <!--<![endif]-->
+  <style>
+    * {
+      box-sizing: border-box;
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+    }
+
+    a[x-apple-data-detectors] {
+      color: inherit !important;
+      text-decoration: inherit !important;
+    }
+
+    #MessageViewBody a {
+      color: inherit;
+      text-decoration: none;
+    }
+
+    p {
+      line-height: inherit
+    }
+
+    .desktop_hide,
+    .desktop_hide table {
+      mso-hide: all;
+      display: none;
+      max-height: 0px;
+      overflow: hidden;
+    }
+
+    @media (max-width: 670px) {
+
+      .desktop_hide table.icons-inner,
+      .social_block.desktop_hide .social-table {
+        display: inline-block !important;
+      }
+
+      .icons-inner {
+        text-align: center;
+      }
+
+      .icons-inner td {
+        margin: 0 auto;
+      }
+
+      .image_block img.big,
+      .row-content {
+        width: 100% !important;
+      }
+
+      .mobile_hide {
+        display: none;
+      }
+
+      .stack .column {
+        width: 100%;
+        display: block;
+      }
+
+      .mobile_hide {
+        min-height: 0;
+        max-height: 0;
+        max-width: 0;
+        overflow: hidden;
+        font-size: 0px;
+      }
+
+      .desktop_hide,
+      .desktop_hide table {
+        display: table !important;
+        max-height: none !important;
+      }
+
+      .reverse {
+        display: table;
+        width: 100%;
+      }
+
+      .reverse .column.first {
+        display: table-footer-group !important;
+      }
+
+      .reverse .column.last {
+        display: table-header-group !important;
+      }
+
+      .row-13 td.column.first .border {
+        padding-left: 20px;
+        padding-right: 20px;
+        border-top: 0;
+        border-right: 0px;
+        border-bottom: 0;
+        border-left: 0;
+      }
+
+      .row-13 td.column.last .border {
+        padding-left: 0;
+        padding-right: 0;
+        border-top: 0;
+        border-right: 0px;
+        border-bottom: 0;
+        border-left: 0;
+      }
+    }
+  </style>
+</head>
+<body style="margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none; background-color: #0f1337;">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tbody>
+  <tr>
+    <td>
+      <table id="content_footer" border="0" cellpadding="0" cellspacing="0" width="580">
+        <tr>
+          <td height="60"></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  <tr>
+    <td align="center">
+      <table border="0" cellpadding="0" cellspacing="0" width="580"
+             style="color: rgb(34, 34, 34); font-family: arial, sans-serif; font-size: 12.8px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255,255,255);">
+        <tbody>
+        <tr id="header" valign="top">
+          <td colspan="3" style="font-family: arial, sans-serif; margin: 0px;">
+            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+              <tbody>
+              <tr valign="top">
+                <td width="130px;" style="font-family: arial, sans-serif; margin: 0px;"><a
+                    href="http://azzurracapital.com.br/"><img
+                      src="http://dev.azzurracapital.com.br/images/azzurra__email-address__hero.jpg"
+                      width="580" class="CToWUd"></a></td>
+              </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+        <tr id="main">
+          <td width="12"
+              style="font-family: arial, sans-serif; margin: 0px; border-left-width: 1px; border-left-style: solid; border-left-color: rgb(221, 221, 221); background: url('https://ci4.googleusercontent.com/proxy/N0L944HCjnvwWRXBljU4ZfipjEACBbT74c9tnfqjqcKRHYJAfxxlQr8rOPL_ETHsSXpPLpwREJOI33BTHDZw2jjZ=s0-d-e1-ft#http:///i/scr/scr_emailLeftBorder_13wx1h.gif') 0% 50% repeat-y;"
+              bgcolor="url('https"><img src="http://azzurracapital.com.br/img/newsletters/email-resposta/unnamed-3.gif"
+                                        border="0" alt="" class="CToWUd"></td>
+          <td width="530"
+              style="font-family: arial, sans-serif; margin: 0px; width: 530px; word-wrap: break-word; padding: 12px;">
+            <table width="100%">
+              <tbody>
+              <tr>
+                <td style="font-family: arial, sans-serif; margin: 0px;"><span
+                    style="font-family:'Helvetica Neue Light', Helvetica; font-size:28px; color:rgb(0, 121, 193); font-weight:300; ">
+                    <p style="margin: 11px 0px; padding: 0px; font-size: 22.4px; color: rgb(0, 69, 124);">Um novo contato se cadastrou no site</p>
+                    </span>
+                  <p style="margin: 11px 0px; padding: 0px;">O contato se cadastrou no site através do fomulário de
+                    contato:</p>
+                  <p style="margin: 11px 0px; padding: 0px;"><strong>Nome:</strong> [your-name]</p>
+                  <p style="margin: 11px 0px; padding: 0px;"><strong>E-mail:</strong> [your-email]</p>
+                  <p style="margin: 11px 0px; padding: 0px;"><strong>Telefone:</strong> [tel-0001-contato]</p>
+                  <p style="margin: 11px 0px; padding: 0px;"><strong>Assunto:</strong> [your-subject]</p>
+                  <p style="margin: 11px 0px; padding: 0px;"><strong>Mensagem:</strong><br><br>
+                    [your-message]</p>
+              </tr>
+              </tbody>
+            </table>
+          </td>
+          <td width="12"
+              style="font-family: arial, sans-serif; margin: 0px; border-right-width: 1px; border-right-style: solid; border-right-color: rgb(221, 221, 221); background: url('https://ci6.googleusercontent.com/proxy/tJTSIiFFJTeNg6bI5dXgfivt4w1ZtT2c02j-j3GFrBF0ZzIyiv2nvAOyuL-RION8gcGrrOpUgynLsDbpQcdYS5EVlQ=s0-d-e1-ft#http:///i/scr/scr_emailRightBorder_13wx1h.gif') 0% 50% repeat-y;"
+              bgcolor="url('https"><img src="http://azzurracapital.com.br/img/newsletters/email-resposta/unnamed-3.gif"
+                                        border="0" alt="" class="CToWUd"></td>
+        </tr>
+        </tbody>
+      </table>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <table id="content_footer" border="0" cellpadding="0" cellspacing="0" width="580">
+        <tr>
+          <td height="60"></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  </tbody>
+</table>
+</body>
+</html>
