7f402e01fb9dc704d851baedfc72930c1ddd8d60
Daniel Lima <familiasilva.daniel@gmail.com>
2023-01-25 20:01:09 -0300

    refactor(investmentpolicysection): add json data file to InvestmentPolicySection component

--- src/templates/Home/components/InvestmentPolicySection/index.tsx
+++ src/templates/Home/components/InvestmentPolicySection/index.tsx
@@ -4,6 +4,8 @@ import { Element, scroller } from 'react-scroll/modules';
 import { useBreakpointType } from '@root/modules/hooks';
 import { getVisibility } from '@root/modules/utils';
 
+import JSONData from '@content/home/pt/InvestmentPolicySection-JSON-Content.json';
+
 import InvestmentPolicyMandala from '@components/Icons/InvestmentPolicyMandala';
 import Container from '@components/Layout/Container';
 import Title from '@components/Title';
@@ -73,7 +75,7 @@ function InvestmentPolicySection() {
           >
             <Title
               className={'mb-8 sm:mb-24 md:mb-14'}
-              label={['Política de', 'Investimento']}
+              label={JSONData.title as never}
             />
             <div
               className={
@@ -94,113 +96,45 @@ function InvestmentPolicySection() {
             name={'investment-policy-steps'}
             className="w-full mobile-landscape:w-7/12 lg:w-6/12 flex flex-col gap-8 px-6 xl:px-0"
           >
-            <Element
-              name={'investment-step-1'}
-              style={getVisibility(1, investmentStep, { breakpoint }).style}
-              className={'transition-all'}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              <InvestmentPolicyItem
-                title={{
-                  text: '01 | ANÁLISE DO PERFIL DO INVESTIDOR E CARTEIRA ATUAL',
-                  color: 'gold-100'
-                }}
-                description={
-                  'Diagnóstico inicial que será feita a avaliação da expectativa de prazo, liquidez e tolerância ' +
-                  'ao risco do investidor, para que o planejamento de investimentos possa estar alinhado com as ' +
-                  'expectativas de maneira realista, na construção de um portfólio personalizado, e que esteja ' +
-                  'adequado a necessidade dos objetivos.'
-                }
-              />
-            </Element>
-
-            <Element
-              name={'investment-step-2'}
-              style={getVisibility(2, investmentStep, { breakpoint }).style}
-              className={'transition-all'}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              <InvestmentPolicyItem
-                title={{
-                  text: '02 | ANÁLISE DO CENÁRIO MACRO E MICROECONÔMICO',
-                  color: 'navy-blue'
-                }}
-                description={
-                  'Definição da Política de Investimento eficaz, para direcionar e disciplinar ' +
-                  'investimentos ao longo do tempo, de acordo com ambiente econômico.'
-                }
-              />
-            </Element>
-
-            <Element
-              name={'investment-step-3'}
-              style={getVisibility(3, investmentStep, { breakpoint }).style}
-              className={'transition-all'}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              <InvestmentPolicyItem
-                title={{
-                  text: '03 | ELABORAÇÃO E APRESENTAÇÃO DO PORTFÓLIO',
-                  color: 'light-blue'
-                }}
-                description={
-                  'Respeitando a Política de Investimentos elaborada, serão avaliados os ativos ' +
-                  'que poderão compor o portfólio, que será personalizado conforme a necessidade ' +
-                  'de liquidez e o perfil do risco, sendo apresentado as estratégias a serem adotadas ' +
-                  'para atingir o objetivo do investidor.'
-                }
-              />
-            </Element>
-
-            <Element
-              name={'investment-step-4'}
-              style={getVisibility(4, investmentStep, { breakpoint }).style}
-              className={'transition-all'}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              <InvestmentPolicyItem
-                title={{
-                  text: '04 | IMPLEMENTAÇÃO DA CARTEIRA',
-                  color: 'opaque-gold'
-                }}
-                description={
-                  'A execução da implementação é integralmente realizada pela Azzurra Capital, no formato ' +
-                  'de carteira administrada ou fundo exclusivo, observado o disposto na Instrução CVM 558/2015.'
-                }
-              />
-            </Element>
-
-            <Element
-              name={'investment-step-5'}
-              style={getVisibility(5, investmentStep, { breakpoint }).style}
-              className={'transition-all'}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              <InvestmentPolicyItem
-                title={{
-                  text: '05 | ACOMPANHAMENTO E REUNIÕES PERIÓDICAS COM O INVESTIDOR',
-                  color: 'brown'
-                }}
-                description={
-                  <>
-                    {'O cliente terá acesso ao seu portfólio de maneira eletrônica no site da Azzurra Capital, ' +
-                      'para que possa acompanhar o desempenho de seus investimentos, bem como retirar ' +
-                      'seu extrato pela plataforma.'}
-                    <br />
-                    <br />
-                    {'É realizado o monitoramento constante das carteiras dos clientes por meio dos ' +
-                      'especialistas em investimentos e é estabelecida, em comum acordo com o cliente, ' +
-                      'a periodicidade das reuniões para avaliação da estratégia adotada e dos ajustes ' +
-                      'do portfólio, quando necessário'}
-                  </>
-                }
-              />
-            </Element>
+            {JSONData.steps.map((step) => {
+              return (
+                <Element
+                  key={step.id}
+                  name={step.name}
+                  style={
+                    getVisibility(step.id + 1, investmentStep, { breakpoint })
+                      .style
+                  }
+                  className={'transition-all'}
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <InvestmentPolicyItem
+                    title={{
+                      text: step.title,
+                      color: step.color as never
+                    }}
+                    description={
+                      <>
+                        {step.description.map((paragraph, idx) => {
+                          return (
+                            <>
+                              {paragraph}
+                              {idx >= step.description.length ? null : (
+                                <>
+                                  <br />
+                                  <br />
+                                </>
+                              )}
+                            </>
+                          );
+                        })}
+                      </>
+                    }
+                  />
+                </Element>
+              );
+            })}
           </Element>
         </div>
       </Container>
